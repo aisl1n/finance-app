f02d0fbcdcb1a55bdd47317f01d34d64c50510d3
aisl1n <aislanarchy@gmail.com>
2024-12-03 22:59:59 -0300

    feat: modify qr code scanner to ensure user can scanner only when is logged

---
+++ app/scanner/_components/qr-code-scanner.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Scanner,
+  useDevices,
+  IDetectedBarcode,
+} from "@yudiel/react-qr-scanner";
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import { upsertTransaction } from "@/app/_actions/upsert-transaction";
+import TableResults from "./table-results";
+import { Button } from "react-day-picker";
+import Navbar from "@/app/_components/navbar";
+import axios from "axios";
+
+type ScannedDataType = {
+  commerceName: string;
+  amount: number;
+  paymentMethod: string;
+  products: Array<{
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+  }>;
+  key?: string;
+};
+
+const QrCodeScanner = () => {
+  const [scannedData, setScannedData] = useState<ScannedDataType>({
+    commerceName: "",
+    amount: 0,
+    paymentMethod: "",
+    products: [],
+  });
+  const useIsScannedDataEmpty = (scannedData: ScannedDataType) => {
+    return (
+      scannedData.commerceName === "" &&
+      scannedData.amount === 0 &&
+      scannedData.paymentMethod === "" &&
+      scannedData.products.length === 0
+    );
+  };
+
+  const devices = useDevices();
+  const isScannedDataEmpty = useIsScannedDataEmpty(scannedData);
+
+  const resetScannedData = () => {
+    setScannedData({
+      commerceName: "",
+      amount: 0,
+      paymentMethod: "",
+      products: [],
+    });
+  };
+
+  const drawFrame = (
+    detectedCodes: IDetectedBarcode[],
+    ctx: CanvasRenderingContext2D,
+  ) => {
+    detectedCodes.forEach((code) => {
+      const { x, y, width, height } = code.boundingBox;
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(x, y, width, height);
+    });
+  };
+
+  const handleScan = async (url: string) => {
+    try {
+      const response = await axios.get(
+        "https://nf-api-server.vercel.app/proxy",
+        {
+          params: { url },
+        },
+      );
+      console.log("Data fetched:", response.data);
+      setScannedData(response.data);
+      // setMessage(true);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleCancel = () => {
+    resetScannedData();
+  };
+
+  const paymentMethodChecker = (paymentMethod: string) => {
+    return paymentMethod.includes("CARTÃO DE CRÉDITO")
+      ? TransactionPaymentMethod.CREDIT_CARD
+      : TransactionPaymentMethod.PIX;
+  };
+
+  const handleSave = async () => {
+    if (isScannedDataEmpty) return;
+    // Criar verificação para se o ID(kEY DA NOTA FISCAL) já existe
+    try {
+      upsertTransaction({
+        name: scannedData.commerceName,
+        amount: scannedData.amount,
+        type: TransactionType.EXPENSE,
+        category: TransactionCategory.FOOD,
+        paymentMethod: paymentMethodChecker(scannedData.paymentMethod),
+        id: scannedData.key,
+        date: new Date(),
+      });
+      resetScannedData();
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
+  };
+  return (
+    <>
+      {isScannedDataEmpty ? (
+        <div className="flex items-center justify-center">
+          <div className="mx-2 mt-16 flex rounded-md p-2 sm:h-[40rem] sm:w-[40rem] sm:flex-row">
+            <Scanner
+              constraints={{
+                facingMode: "environment",
+                deviceId: devices[3]?.deviceId,
+                frameRate: { ideal: 30, max: 60 },
+              }}
+              onScan={(result) => {
+                if (result[0]?.rawValue) {
+                  handleScan(result[0].rawValue);
+                }
+              }}
+              components={{ tracker: drawFrame }}
+            />
+          </div>
+        </div>
+      ) : (
+        <div>
+          {/* <RenderMessage setMessage={setMessage} message={message} /> */}
+          <div className="pb-20">
+            <TableResults scannedData={scannedData} />
+            <div className="flex flex-row justify-end gap-2 p-4">
+              <Button
+                className="bg-rose-600 font-bold text-inherit hover:bg-rose-800"
+                onClick={() => handleCancel()}
+              >
+                Cancelar
+              </Button>
+              <Button
+                className="bg-emerald-500 font-bold text-inherit hover:bg-emerald-800"
+                onClick={() => handleSave()}
+              >
+                Salvar compra
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+      <Navbar />
+    </>
+  );
+};
+
+export default QrCodeScanner;
