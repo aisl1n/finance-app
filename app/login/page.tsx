9082f4b12ec1d0814517a985740d0fa00afac404
aisl1n <aislanarchy@gmail.com>
2024-12-06 15:37:31 -0300

    feat: update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,10 +4,11 @@ import { LogInIcon } from "lucide-react";
 import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { Dancing_Script } from "next/font/google";
+import { Kanit } from "next/font/google";
 
-const dancingScript = Dancing_Script({
+const kanit = Kanit({
   subsets: ["latin-ext"],
+  weight: ["400", "700"],
 });
 
 const LoginPage = async () => {
@@ -17,37 +18,36 @@ const LoginPage = async () => {
     redirect("/");
   }
   return (
-    <div className="mx-auto flex max-w-[550px] flex-col justify-center p-8">
-      <div className="mb-8 flex items-center justify-center gap-4 text-center">
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-28">
+      <div className="flex items-center justify-center gap-2 text-center">
         <Image
           src="/logo.svg"
           width={60}
           height={39}
           alt="Logo da marca Finance AI"
         />
-        <h1
-          className={`${dancingScript.className} neon-text text-5xl font-bold text-white`}
-        >
+        <h1 className={`${kanit.className} neon-text text-5xl text-white`}>
           Finance App
         </h1>
       </div>
-      <div className="mt-20 flex flex-col items-center justify-center">
-        <div>
-          <h2 className="mb-3 text-2xl font-bold">Bem vindo(a)!</h2>
-          <p className="mb-8 text-muted-foreground">
+      <div className="flex flex-col">
+        <div className="p-8">
+          <h2 className="mb-2 text-2xl font-bold">Bem vindo(a)!</h2>
+          <p className="text-justify text-muted-foreground">
             A Finance App é uma solução para sua vida financeira. Com ela, você
             poderá controlar suas finanças de forma simples e eficiente.
           </p>
         </div>
-        <SignInButton>
-          <Button variant="outline">
-            <LogInIcon className="mr-2" />
-            Fazer login ou criar conta
-          </Button>
-        </SignInButton>
+        <div className="flex items-center justify-center">
+          <SignInButton>
+            <Button variant="outline">
+              <LogInIcon />
+              Fazer login ou criar conta
+            </Button>
+          </SignInButton>
+        </div>
       </div>
-
-      <div className="mt-12 flex h-96 flex-col justify-end text-center text-sm text-muted-foreground">
+      <div className="flex flex-col text-center text-sm text-muted-foreground">
         <p>
           &copy; {new Date().getFullYear()} Finance App. Todos os direitos
           reservados.
