8a3f3b4113818252e29aed8a19e9b2310a6ab7dd
aisl1n <aislanarchy@gmail.com>
2024-12-06 15:48:01 -0300

    feat: add title and update style

--- app/scanner/page.tsx
+++ app/scanner/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import QrCodeScanner from "./_components/qr-code-scanner";
 import { redirect } from "next/navigation";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 const ScannerPage = async () => {
   const { userId } = await auth();
@@ -9,7 +10,15 @@ const ScannerPage = async () => {
     redirect("/login");
   }
 
-  return <QrCodeScanner />;
+  return (
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Scanner</h1>
+        <AddTransactionButton />
+      </div>
+      <QrCodeScanner />
+    </div>
+  );
 };
 
 export default ScannerPage;
